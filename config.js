0497727d02b61ad28bc17b2f464665e7b13d2691
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2013-08-31 17:37:08 +0200

    Upgrade Version

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 module.exports = config;
 
 config.TITLE    =   "Picchio File Uploader";
-config.VERSION  =   "1.0A";
+config.VERSION  =   "1.0";
 config.URL     =   "https://www.filepicker.io/api/path/storage/";
 config.API      =   ""; //INSERT HERE YOUR API KEY
 
