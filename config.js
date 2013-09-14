6f98469fa52ac16d17a7f53876f34af34004d26f
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2013-09-15 01:45:42 +0200

    Removed config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-module.exports = config;
-
-config.TITLE    =   "Picchio File Uploader";
-config.VERSION  =   "1.0";
-config.URL     =   "https://www.filepicker.io/api/store/S3?key=";
-config.API      =   ""; //INSERT HERE YOUR API KEY
-
